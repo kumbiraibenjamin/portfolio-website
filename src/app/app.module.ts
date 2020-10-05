0604b47d14570257db7e77069ccb52c89ee728ab
kumbiraibenjamin <32431147+kumbiraibenjamin@users.noreply.github.com>
2020-10-05 21:06:06 +0200

    added docs folder for git hub pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AppComponent } from "./app.component";
+import { CoreModule } from "./core/core.module";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    CoreModule,
-
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CoreModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
+// https://github.com/kumbiraibenjamin/portfolio-website
+// ng build--prod--base-href "https://kumbiraibenjamin.github.io/portfolio-website/" --output-path=docs
